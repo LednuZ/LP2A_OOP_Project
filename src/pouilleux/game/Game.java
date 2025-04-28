848388812f38bdfe9b28ff9c6bc2a920192f46ce
abluvin <153931513+abluvin@users.noreply.github.com>
2025-04-28 19:19:00 +0200

    Update Game.java

--- src/pouilleux/game/Game.java
+++ src/pouilleux/game/Game.java
@@ -11,42 +11,40 @@ public class Game {
     private int currentPlayer ;
 
     public Game(String name ){
-
-        // Creation of 4 players: one user and 3 bots 
-        this.players.add(new Player(name));
-        this.players.add(new Bot("Player 2"));
-        this.players.add(new Bot("Player 3"));
-        this.players.add(new Bot("Player 4"));
-        
-        //creating the deck 
-        Deck deck = new Deck() ; 
-
-        //distributing the cards 
-        while (!deck.isEmpty()){
-            for (Player player : players) {               
-                Card card = deck.drawCard() ;
-                player.getHand().addCard(card) ; 
-            } 
-        }
+    	// Creation of 4 players: one user and 3 bots 
+	    this.players.add(new Player(name));
+	    this.players.add(new Bot("Player 2"));
+	    this.players.add(new Bot("Player 3"));
+	    this.players.add(new Bot("Player 4"));
+	    
+	    //creating the deck 
+	    Deck deck = new Deck() ; 
+	
+	    //distributing the cards 
+	    while (!deck.isEmpty()){
+	        for (Player player : players) {               
+	            Card card = deck.drawCard() ;
+	            player.getHand().addCard(card) ; 
+	        } 
+	    }
 
     }
 
-    	/**
-	 * Select a random dealer to start the game 
-	 */
+    /**
+	* Select a random dealer to start the game 
+	*/
 	public void selectRandomDealer() {
         Random random = new Random();
         int first_player = random.nextInt(5); 
 
-
-
-
+        
+        
+        
+        
+        
     }
-
-
-
-
-    	/**
+	
+	/**
 	 * Returns true if the only card in play is the jack of spades or 3 players are finished
 	 * @return a boolean value
 	 */
